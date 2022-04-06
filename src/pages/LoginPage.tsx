698fd1992d0fc6ea17bb8721dd008768206046f2
Yujin Oh <yujin2e@ewhain.net>
2022-04-06 09:51:00 +0900

    commit

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { TextField } from '../components/TextField';
 
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return <div className="m-4">
+    <div className='text-3xl font-bold mb-10'>로그인</div>
+
+    <div>
+      <TextField label="이메일" placeholder="이메일을 입력해주세요."/>
+      <TextField label="비밀번호" placeholder="비밀번호를 입력해주세요." type="password"/>
+    </div>
+
+    <div className="mt-10">
+      <div className='bg-gray-800 text-white text-center py-4'>로그인</div>
+      <div className='text-center py-4 text-gray-800 border border-gray-800 mt-2'>회원가입</div>
+    </div>
+  </div>;
 };
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { TextField } from '../components/TextField';
 
 export const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return <div className="m-4">
+    <div className='text-3xl font-bold mb-10'>회원가입</div>
+
+    <div>
+    <TextField label="타이틀" placeholder="내용을 입력해주세요."/>
+    <TextField label="타이틀" placeholder="내용을 입력해주세요."/>
+    <TextField label="타이틀" placeholder="내용을 입력해주세요."/>
+    <TextField label="타이틀" placeholder="내용을 입력해주세요."/>
+   </div>
+
+    <div className="mt-10">
+    <div className='text-center py-4 text-gray-800 border border-gray-800 mt-2'>회원가입</div>
+    </div>
+  </div>;
 };
