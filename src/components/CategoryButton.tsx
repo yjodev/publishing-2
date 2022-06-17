43507311136502a94708b6d856ed40a55a8f7ef9
yjodev <101042643+yjodev@users.noreply.github.com>
2022-06-17 13:44:21 +0900

    added components

--- src/components/CategoryButton.tsx
+++ src/components/CategoryButton.tsx
@@ -6,15 +6,15 @@ type CategoryButtonProps = {
     onClick: (index: number) => void;
 }
 
-export const CategoryButton:React.FC<CategoryButtonProps> = ({ category, onClick }) => {
+export const CategoryButton: React.FC<CategoryButtonProps> = ({ category, onClick }) => {
     return (
         <div onClick={() => onClick(category.id)}>
-        <img 
-        src="https://images.unsplash.com/photo-1535016120720-40c646be5580?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        alt=""
-        className="w-20 h-20 object-cover rounded-full"
-        />
-        <div className="text-center">{category.label}</div>
-      </div>
+            <img
+                src={category.image}
+                alt=""
+                className="w-20 h-20 object-cover rounded-full"
+            />
+            <div className="text-center">{category.label}</div>
+        </div>
     );
 };
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -6,18 +6,18 @@ const IMAGE_URL = 'https://image.tmdb.org/t/p/w200';
 type MovieCardProps = {
     movie: Movie;
 }
-export const MovieCard:React.FC<MovieCardProps> = ({movie}) => {
+export const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
     return (
         <div className="flex space-x-4">
-        <img 
-          src={`${IMAGE_URL}${movie.poster_path}`}
-          alt="" 
-          className="w-20 h-30 object-cover rounded-md"/>
-        <div>
-          <div className="text-lg font-semibold">{movie.title}</div>
-          <div className="text-gray-700 truncate-2-lines">{movie.overview}</div>
-          <div className="text-gray-500">{`평점 : ${movie.vote_average}`}</div> 
+            <img
+                src={`${IMAGE_URL}${movie.poster_path}`}
+                alt=""
+                className="w-20 h-30 object-cover rounded-md" />
+            <div>
+                <div className="text-lg font-semibold">{movie.title}</div>
+                <div className="text-gray-700 truncate-2-lines">{movie.overview}</div>
+                <div className="text-gray-500">{`평점 : ${movie.vote_average}`}</div>
+            </div>
         </div>
-      </div>
     );
 };
