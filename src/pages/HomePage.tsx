e76924cf3eb9518e6b16f49b81d71de534d75c72
yjodev <101042643+yjodev@users.noreply.github.com>
2022-06-17 13:44:35 +0900

    added pages

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { CategoryButton } from '../components/CategoryButton';
 import { MovieCard } from '../components/MovieCard';
+import nowPlayingImg from '../assets/images/image2.jpg';
+import topRatedImg from '../assets/images/image3.jpg';
+import upcomingImg from '../assets/images/image4.jpg';
+
 const API_KEY = 'd7cfdd1572e06bb0e7952f1b466a0ee5';
+
 
 export type Category = {
   id: number;
   label: string;
   url: string;
- };
- 
- const CATEGORY_LIST = [
-  { id: 0, label: '인기있는영화', url: '/popular' },
-  { id: 1, label: '현재 상영작', url: '/now_playing' },
-  { id: 2, label: '별점 높은 영화', url: '/top_rated' }
- ];
+  image: string;
+};
+
+const CATEGORY_LIST = [
+  { id: 0, label: '현재 상영작', url: '/now_playing', image: nowPlayingImg },
+  { id: 1, label: '별점 높은 영화', url: '/top_rated', image: topRatedImg },
+  { id: 2, label: '개봉 예정작', url: '/upcoming', image: upcomingImg },
+];
 
 export type Movie = {
   id: number;
@@ -21,24 +27,24 @@ export type Movie = {
   overview: string;
   poster_path: string;
   vote_average: number;
- };
- 
+};
+
 
 export const HomePage = () => {
-  const [isLoading,setIsLoading] = useState(true);
-  const [movies,setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState<Movie[]>([]);
   const [categoryIndex, setCategoryIndex] = useState(0);
 
-  const setCategory = (index:number) => {
+  const setCategory = (index: number) => {
     setCategoryIndex(index);
   }
 
 
-  const getData = async (categoryIndex:number) => {
+  const getData = async (categoryIndex: number) => {
     const url = `https://api.themoviedb.org/3/movie${CATEGORY_LIST[categoryIndex].url}?api_key=${API_KEY}&language=ko-KR&page=1`
     const response = await fetch(url);
     console.log(response);
-    if (response.status == 200 ) {
+    if (response.status == 200) {
       // 데이터 잘 왔을 때 실행할 내용
       const data = await response.json();
       console.log(data);
@@ -50,17 +56,18 @@ export const HomePage = () => {
     setIsLoading(false);
   }
 
-useEffect(() => {
-  getData(categoryIndex);
-}, [categoryIndex])
+  useEffect(() => {
+    getData(categoryIndex);
+  }, [categoryIndex])
 
-    return (
+
+  return (
     <div className='m-4 space-y-10'>
       <div className="space-y-4">
         <div className="text-2xl font-bold ">New</div>
-        <img 
-          src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt="" 
+        <img
+          src="https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          alt=""
           className="w-full h-72 object-cover rounded-2xl"
         />
       </div>
@@ -69,66 +76,64 @@ useEffect(() => {
         <div className="text-2xl font-bold">Category</div>
 
         <div className="flex space-x-6">
-          {CATEGORY_LIST.map((data) => (<CategoryButton category={data} onClick={setCategory}/>))}
-
+          {CATEGORY_LIST.map((data) => (<CategoryButton category={data} onClick={setCategory} />))}
 
         </div>
-
       </div>
 
-{/*
-      <div>
-        <div className="text-2xl font-bold mb-4">List</div>
-        
-        <div className="border p-4 rounded-md">
-          <div>
-            <img 
-              src="https://images.unsplash.com/photo-1585032226651-759b368d7246?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2hpbmVzZSUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
-              alt=""
-              className="w-full h-60 object-cover rounded-2xl"
+
+      {/* <div>
+          <div className="text-2xl font-bold mb-4">List</div>
+
+          <div className="border p-4 rounded-md">
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1585032226651-759b368d7246?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2hpbmVzZSUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
+                alt=""
+                className="w-full h-60 object-cover rounded-2xl"
               />
-            <div className="mt-4">
-              <div className="text-lg font-semibold">Card Title</div>
-              <div className="text-gray-500">
-                Card contents. Asfnjawfnjsdfn nejkdsnfklsdnkl.
-              </div>
-              <div className="mt-4 flex justify-end space-x-3">
-                <div className="bg-gray-800 rounded-md text-white text-center py-2.5 px-4">
-                  Button
+              <div className="mt-4">
+                <div className="text-lg font-semibold">Card Title</div>
+                <div className="text-gray-500">
+                  Card contents. Asfnjawfnjsdfn nejkdsnfklsdnkl.
                 </div>
-                <div className="bg-gray-800 rounded-md text-white text-center py-2.5 px-4">
-                  Button
+                <div className="mt-4 flex justify-end space-x-3">
+                  <div className="bg-gray-800 rounded-md text-white text-center py-2.5 px-4">
+                    Button
+                  </div>
+                  <div className="bg-gray-800 rounded-md text-white text-center py-2.5 px-4">
+                    Button
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    */}
-     
+        </div> */}
+
 
       <div className="space-y-4">
         <div className="text-2xl font-bold">Today's Special</div>
         {!isLoading && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
-      
 
         <div className="border-b-2"></div>
 
         <div className="space-y-4">
-          <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60" 
-          alt="" 
-          className="w-full h-60 object-cover rounded-2xl"/>
-          <div className="space-y-2">
-            <div className="text-lg font-semibold">이화여대 아맛나 식당</div>
+          <div className="text-2xl font-bold">Today's Actor</div>
+
+          <div className="flex space-x-4">
+            <img
+              src="https://images.unsplash.com/photo-1620845417099-57e9635a914c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8c2lsdWV0dGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+              alt=""
+              className="w-20 h-30 object-cover rounded-md" />
             <div>
-              <div className="text-gray-700">서울특별시 서대문구 대현동 11-1</div>
-              <div className="text-gray-700">02-1234-5678 </div>
-              <div className="text-gray-700">MON-SUN 12:00PM - 9:00PM</div>
+              <div className="text-lg font-semibold">한소희</div>
+              <div className="text-gray-700">말이 필요 없는 최고의 배우</div>
+              <div className="text-gray-500">알고있지만 (2021) </div>
             </div>
           </div>
-          <div className="bg-gray-900 rounded-3xl text-white text-center py-2.5 px-4">
-                  네이버 지도로 길찾기
-                </div>
+
+          <div className="border-b-2"></div>
+
         </div>
       </div>
     </div>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -12,58 +12,55 @@ export const LoginPage = () => {
   const users = data.users;
 
   const checkUser = () => {
+
     if (email === '' || password === '') {
       alert('아이디와 비밀번호를 입력해주세요');
       return;
-    } 
+    }
 
 
-    for(let i=0 ; i<users.length ; i++) {
+    for (let i = 0; i < users.length; i++) {
       if (email === users[i].email && password === users[i].password) {
         alert("로그인 성공");
         replace('/'); // 현재 상위 페이지를 대체
         return;
-      } else if ( email === users[i].email && password !== users[i].password ){
-          alert('비밀번호가 틀립니다');
-          return;
-        }
       }
     }
     alert("로그인 실패");
-  
+  }
 
   return <div className="m-4 space-y-20">
     <div className=' text-3xl font-bold'>로그인</div>
 
     <div className="space-y-4">
-      <TextField 
-      label={"이메일"} 
-      placeholder="이메일을 입력해주세요."
-      value={email}
-      onChange={(event) => {
+      <TextField
+        label={"이메일"}
+        placeholder="이메일을 입력해주세요."
+        value={email}
+        onChange={(event) => {
           setEmail(event.target.value);
-          }}
-          />
-      <TextField 
-        label={"비밀번호"} 
-        placeholder="비밀번호를 입력해주세요." 
+        }}
+      />
+      <TextField
+        label={"비밀번호"}
+        placeholder="비밀번호를 입력해주세요."
         value={password}
         onChange={(event) => {
           setPassword(event.target.value);
-          }}
+        }}
         type="password"
       />
     </div>
 
     <div >
-      <div 
-      className='bg-gray-800 text-white text-center rounded-md py-4' 
-      onClick={() => {
-        checkUser();
-      }}>
+      <div
+        className='bg-gray-800 text-white text-center rounded-md py-4'
+        onClick={() => {
+          checkUser();
+        }}>
         로그인</div>
-      <div 
-      className='text-center py-4 text-gray-800 border border-gray-800 rounded-md mt-2'>회원가입</div>
+      <div
+        className='text-center py-4 text-gray-800 border border-gray-800 rounded-md mt-2'>회원가입</div>
     </div>
   </div>;
 };
